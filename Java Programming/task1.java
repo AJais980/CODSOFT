974efef631da2e2d871cd6081dda1a1a1859e77d
AJais980 <amanjais980@gmail.com>
2023-10-16 22:51:28 +0530

    Update UI for task 1 of Java Programming

--- Java Programming/task1.java
+++ Java Programming/task1.java
@@ -5,7 +5,10 @@ class task1 {
         int answer, guess;
         final int MAX = 100;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter max number of tries you want to take: ");
+        System.out.println("<===============================================>");
+        System.out.println("\t    Welcome To The Number Game");
+        System.out.println("<===============================================>");
+        System.out.print("\tEnter max tries you want to take: ");
         int maxTries = sc.nextInt();
         String reRun = "yes";
         while (reRun.toLowerCase().equals("yes")) {
@@ -15,22 +18,23 @@ class task1 {
             boolean correct = false;
             while (!correct) {
                 if (usedTries == maxTries) {
-                    System.out.println("You've reached your max number of tries.");
+                    System.out.println("\tYou've reached your max number of tries.");
                     break;
                 }
-                System.out.print("Guess a number between 1 and 100: ");
+                System.out.print("\tGuess a number between 1 and 100: ");
                 guess = sc.nextInt();
                 usedTries++;
                 if (guess > answer) {
-                    System.out.println("Too high, guess again.");
+                    System.out.println("\t\tToo high, guess again.");
                 } else if (guess < answer) {
-                    System.out.println("Too low, guess again.");
+                    System.out.println("\t\tToo low, guess again.");
                 } else {
-                    System.out.println("Yes, you guessed the number.");
+                    System.out.println("\t    Yes, you guessed the number.");
                     correct = true;
                 }
+                System.out.println("<===============================================>");
             }
-            System.out.print("Wanna try again?: ");
+            System.out.print("\t\tWanna try again?: ");
             reRun = sc.next();
         }
         sc.close();
